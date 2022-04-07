db64eab32a230411fef360f1dc7e7da2d3109b48
William Peterson <william@mashey.com>
2022-04-07 13:55:54 +0000

    Adds note for project/client discrepancy, updates client stage to function as transformed table

--- models/transform/projects_unioned.sql
+++ models/transform/projects_unioned.sql
@@ -9,7 +9,12 @@ with
             client_id 
         from base 
         join clients on clients._airbyte_harvest_projects_hashid = base._airbyte_harvest_projects_hashid
-    )
-
+    ),
+    clients_stg as (select * from {{ ref('stg_harvest__clients')}})
 {# maybe want to rename name as well with prefix? #}
+
+-- select * from final left join clients_stg on clients_stg.client_id = final.client_id where clients_stg.client_id is null
+-- SELECT * FROM {{source('raw_harvest', 'harvest_projects')}} WHERE id in (22971527, 28364122) LIMIT 1000
+-- One client missing is called "mashey" in harvest details 
+
 select * from final
