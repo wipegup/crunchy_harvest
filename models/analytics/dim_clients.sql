0d3626f8a8ad616a2a4a413f73de3f9d096aecb1
William Peterson <william@mashey.com>
2022-04-07 14:58:34 +0000

    Adds fact and dim tables

---
+++ models/analytics/dim_clients.sql
@@ -0,0 +1,5 @@
+{%- set rel = ref('stg_harvest__clients') %}
+{%- set except_cols = [] + get_airbyte_cols(rel) %}
+with src as (select * from {{ rel }})
+
+select {{ dbt_utils.star(rel, except=except_cols) }} from src
